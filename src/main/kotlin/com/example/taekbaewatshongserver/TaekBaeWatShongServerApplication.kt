5c0fcbd56753d26392ac4747ad8c4ce7bd07dba4
user <sooooooobinn@gmail.com>
2026-07-07 09:46:42 +0900

    init

---
+++ src/main/kotlin/com/example/taekbaewatshongserver/TaekBaeWatShongServerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.taekbaewatshongserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaekBaeWatShongServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<TaekBaeWatShongServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/taekbaewatshongserver/TaekBaeWatShongServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.taekbaewatshongserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaekBaeWatShongServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
