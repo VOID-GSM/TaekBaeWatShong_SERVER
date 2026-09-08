77ea0ce600f0f2b1c234d14dc3e233f0815eafe0
user <sooooooobinn@gmail.com>
2026-09-08 16:51:31 +0900

    feat: 택배함 구역 추가

---
+++ src/main/kotlin/com/example/taekbaewatshongserver/domain/parcel/entity/Zone.kt
@@ -0,0 +1,10 @@
+package com.example.taekbaewatshongserver.domain.parcel.entity
+
+enum class Zone(val description: String) {
+    A("A 구역 - 정문 입구 측"),
+    B("B 구역 - 중앙 보관함"),
+    C("C 구역 - 대형 화물 구역"),
+    D("D 구역 - 후문 입구 측"),
+    E("E 구역 - 신선 식품/냉장"),
+    F("F 구역 - 기타/임시 보관")
+}
