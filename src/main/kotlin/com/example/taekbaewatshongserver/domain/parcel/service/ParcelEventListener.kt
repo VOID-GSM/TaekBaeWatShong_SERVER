3391efe2b04e68cc976bdf8fd79e4ebf9a5b38e4
user <sooooooobinn@gmail.com>
2026-09-09 09:11:25 +0900

    feat: @EnableAsync 어노테이션 추가

--- src/main/kotlin/com/example/taekbaewatshongserver/domain/parcel/service/ParcelEventListener.kt
+++ src/main/kotlin/com/example/taekbaewatshongserver/domain/parcel/service/ParcelEventListener.kt
@@ -1,19 +1,20 @@
 package com.example.taekbaewatshongserver.domain.parcel.service
 
-import org.springframework.context.event.EventListener
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
+import org.springframework.transaction.event.TransactionPhase
+import org.springframework.transaction.event.TransactionalEventListener
 
 @Component
 class ParcelEventListener {
 
     @Async
-    @EventListener
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     fun handleParcelArrivedEvent(event: ParcelArrivedEvent) {
         val parcel = event.parcel
         val owner = parcel.owner
 
-        // 추후 웹 푸시 알림 기능 구현
+        // DB 트랜잭션 커밋이 성공적으로 완료된 후 별도 스레드에서 비동기로 실행됨
         println("[알림 발생] 수신자: ${owner.name} | 내용: [${parcel.alias}] 택배가 ${parcel.zone?.name ?: "미배정"} 구역에 도착했습니다.")
     }
 }
