ce38590ef22bc8b4db50b38e47ff983fc2071f14
user <sooooooobinn@gmail.com>
2026-09-11 12:27:07 +0900

    refactor: n+1 쿼리 문제 해결

--- src/main/kotlin/com/example/taekbaewatshongserver/domain/parcel/repository/ParcelRepository.kt
+++ src/main/kotlin/com/example/taekbaewatshongserver/domain/parcel/repository/ParcelRepository.kt
@@ -4,6 +4,7 @@ import com.example.taekbaewatshongserver.domain.parcel.entity.Parcel
 import com.example.taekbaewatshongserver.domain.parcel.entity.ParcelStatus
 import com.example.taekbaewatshongserver.domain.parcel.entity.Zone
 import com.example.taekbaewatshongserver.domain.user.entity.User
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
@@ -13,20 +14,26 @@ interface ParcelRepository : JpaRepository<Parcel, Long> {
 
     fun findByInvoiceNumber(invoiceNumber: String): Parcel?
 
+    @EntityGraph(attributePaths = ["owner"])
     fun findAllByOwnerOrderByCreatedAtDesc(owner: User): List<Parcel>
 
+    @EntityGraph(attributePaths = ["owner"])
     fun findAllByOwnerAndStatusOrderByCreatedAtDesc(owner: User, status: ParcelStatus): List<Parcel>
 
+    @EntityGraph(attributePaths = ["owner"])
     fun findAllByOwnerAndStatusAndClaimedAtGreaterThanEqualOrderByCreatedAtDesc(
         owner: User,
         status: ParcelStatus,
         claimedAt: LocalDateTime
     ): List<Parcel>
 
+    @EntityGraph(attributePaths = ["owner"])
     fun findAllByOrderByCreatedAtDesc(): List<Parcel>
 
+    @EntityGraph(attributePaths = ["owner"])
     fun findAllByStatusOrderByCreatedAtDesc(status: ParcelStatus): List<Parcel>
 
+    @EntityGraph(attributePaths = ["owner"])
     @Query("""
         SELECT p FROM Parcel p 
         WHERE p.status = :status 
@@ -38,6 +45,7 @@ interface ParcelRepository : JpaRepository<Parcel, Long> {
         @Param("threeDaysAgo") threeDaysAgo: LocalDateTime
     ): List<Parcel>
 
+    @EntityGraph(attributePaths = ["owner"])
     fun findAllByStatusAndZoneIn(status: ParcelStatus, zones: List<Zone>): List<Parcel>
 
     fun existsByInvoiceNumber(invoiceNumber: String): Boolean
